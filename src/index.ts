1a30f53190f8611814d0f4943a9fb56f1a0cab1f
vsadeghy <v.sadeghy@gmail.com>
2025-11-26 01:27:32 +0330

    third time's a charm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { createServer } from "node:http";
-createServer((_req, res) => {
+const server = createServer((_req, res) => {
 	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hi again! World\n");
-}).listen(3000, () => console.log("Server running at http://localhost:3000/"));
+	res.end("not again world\n");
+});
+server.listen(3000, () => console.log("Server running at http://localhost:3000/"));
+
+function gracefullShutdown() {
+	server.close(() => {
+		console.log("Server stopped");
+		process.exit(0);
+	});
+}
+process.on("SIGTERM", gracefullShutdown);
+process.on("SIGINT", gracefullShutdown);
