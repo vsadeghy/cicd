1945dee2169d3dbf83e8138bdfa5c3333426acd0
vsadeghy <v.sadeghy@gmail.com>
2025-11-26 01:02:29 +0330

    update http response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createServer } from "node:http";
 createServer((_req, res) => {
 	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello World\n");
+	res.end("Goodbye, World\n");
 }).listen(3000, () => console.log("Server running at http://localhost:3000/"));
