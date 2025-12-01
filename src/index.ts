5ad71e2403315ee3017e9e5b33c5d3f6cc91ed27
vsadeghy <v.sadeghy@gmail.com>
2025-12-01 19:20:53 +0330

    add watchtower rolling-timeout and healthcheck for zero-downtime

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const server = createServer((req, res) => {
 		return;
 	}
 	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello World");
+	res.end("Hello World!!!");
 });
 server.listen(3000, () => console.log("Server running at http://localhost:3000/"));
 
